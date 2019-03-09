94b25eeee96c14042363501362d44e72da48749a
hackingclimate <sacha.saint-leger@mail.mcgill.ca>
2019-03-09 22:27:16 +0100

    push before migration

--- cbc-scrolly/hack.js
+++ cbc-scrolly/hack.js
@@ -6,13 +6,28 @@ function hack() {
 
 	let g = svg.append('g')
 
-	let datat0 = [];
-	let datat1 = [];
+	let datat0 = ["LMD-Message-Passing-0.png"];
+	let datat1 = ["LMD-Message-Passing-1A.png"];
 	let datat2 = [];
 	let datat3 = [];
 
 	function update(data) {
+	    let t = d3.transition().duration(250);
+	    // DATA JOIN
+	    // Join new data with old elements, if any.
+	    let image = g.selectAll("g.image")
+		.data(data, function(d) { return d})
 
+	    let imageEnter = image.enter()
+		.append('image')
+		.attr('x', 0)
+		.attr('y', 0)
+		.attr('width', 300)
+		.attr('height', 300)
+		.attr('xlink:href', (d) => d);
+
+	    
+	    
 	}
 
 	let gs = d3.graphScroll()
